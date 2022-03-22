93f41d4a3caa45a671ce88fcddbe982df014a33d
Ahmedgamal012064 <64601021+Ahmedgamal012064@users.noreply.github.com>
2022-03-23 01:10:35 +0200

    Update index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -177,11 +177,7 @@ router.post('/signup-verify-mobile', function(req, res, next) {
     }
 });
 
-router.post('/signup-complete', upload.single('image'),function(req, res, next) { //upload.single('image')
-//     return res.status(200).json({
-// 	'status' : true ,
-// 	'data'    : req.body
-//     });
+router.post('/signup-complete',upload.single('image'),function(req, res, next) { //upload.array('images[]',3)
     User.findOne({email:req.body.email},(err , result)=>{
         if(err){
             return res.status(500).json({
@@ -212,9 +208,9 @@ router.post('/signup-complete', upload.single('image'),function(req, res, next) 
             lat: req.body.lat ? req.body.lat  : 0 ,
             lang: req.body.lang ? req.body.lat  : 0,
         });
-        if(req.file){
-            user.images = req.file.path;
-        }
+	if(req.file){
+	    user.images = req.file.path;
+	}
 //         if(req.files){
 //             let path = '';
 //             req.files.forEach(function(file,index,arr){
